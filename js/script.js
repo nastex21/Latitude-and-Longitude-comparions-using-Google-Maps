d6189964be70be8065b5f96fdb1378901aa1a9e1
Tony Salazar <nasax2000@gmail.com>
2018-08-09 09:57:25 -0500

    Updates
    -Added an adjuster of latitude and latitude degrees.
    -Added a way to compare latitudes and longitudes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 var link = "https://res.cloudinary.com/nasax2000/raw/upload/v1533707950/city-data_o7peii.json";
 var request = new XMLHttpRequest();
+var database;
 request.open('GET', link);
 request.responseType = 'json';
 request.send();
 request.onload = function () {
-    var superHeroes = request.response;
-    console.log(superHeroes);
+    database = request.response;
 }
 
-var mykey = config.MY_KEY;
-console.log(mykey);
-var x = document.getElementById("clientLoc");
-var latCoords, lngCoords, pos, addMarker, arr;
+var x = document.getElementById("clientLat");
+var y = document.getElementById("clientLong");
+var latCoords = 25.761;
+var lngCoords = -80.191;
+var pos, addMarker, arr, map;
+var markers = [];
+var latAdjust = document.getElementById("adjustLat").innerHTML;
+var lngAdjust = document.getElementById("adjustLng").innerHTML;
 
 function getLoc() {
+    deleteMarkers();
+    document.getElementById("adjustLat").innerHTML = 0;
+    document.getElementById("adjustLng").innerHTML = 0;
+    latAdjust = 0;
+    lngAdjust = 0;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -21,22 +30,91 @@ function getLoc() {
     }
 }
 
-function getLats() {
+function adjustLatLng(value) {
+    latAdjust = Number(latAdjust);
+    lngAdjust = Number(lngAdjust);
+    if (value == "posLat") {
+        if (latAdjust >= -10 && latAdjust < 10) {
+            latAdjust++;
+            document.getElementById("adjustLat").innerHTML = latAdjust;
+        }
+    }
+    if (value == "negLat") {
+        if (latAdjust > -10 && latAdjust <= 10) {
+            latAdjust--;
+            document.getElementById("adjustLat").innerHTML = latAdjust;
+        }
+    }
+    if (value == "posLng") {
+        if (lngAdjust >= -10 && lngAdjust < 10) {
+            lngAdjust++;
+            document.getElementById("adjustLng").innerHTML = lngAdjust;
+        }
+    }
+    if (value == "negLng") {
+        if (lngAdjust > -10 && lngAdjust <= 10) {
+            lngAdjust--;
+            document.getElementById("adjustLng").innerHTML = lngAdjust;
+        }
+    }
 
 }
 
+function getLats() {
+    deleteMarkers();
+    latAdjust = Number(latAdjust);
+    var latitude = latAdjust + latCoords;
+    latitude = latitude.toFixed(3)
+
+    for (var i = 0; i < database.length; i++) {
+        if (latitude >= Math.floor(database[i].lat) && latitude <= Math.ceil(database[i].lat)) {
+            var pos = {
+                coords: {
+                    lat: JSON.parse(database[i].lat),
+                    lng: JSON.parse(database[i].lng)
+                },
+                content: database[i].city + ", " + database[i].country
+            };
+            addMarker(pos);
+        }
+    }
+}
+
 function getLngs() {
+    deleteMarkers();
+    lngAdjust = Number(lngAdjust);
+    var longitude = lngAdjust + lngCoords;
+    longitude = longitude.toFixed(3)
+
+    for (var i = 0; i < database.length; i++) {
+        num = JSON.parse(database[i].lng);
+        num = Number(num);
+        if (longitude <= Math.ceil(num) && longitude >= Math.floor(num)) {
+            var pos = {
+                coords: {
+                    lat: JSON.parse(database[i].lat),
+                    lng: JSON.parse(database[i].lng)
+                },
+                content: database[i].city + ", " + database[i].country
+            };
+            addMarker(pos);
+        }
+    }
 
 }
 
 function showPosition(position) {
-    latCoords = position.coords.latitude;
-    lngCoords = position.coords.longitude;
+    coordsLat = position.coords.latitude;
+    latCoords = Number(coordsLat.toFixed(3));
+    coordsLng = position.coords.longitude;
+    lngCoords = Number(coordsLng.toFixed(3));
     pos = {
-        lat: latCoords,
-        lng: lngCoords
+        coords: {
+            lat: latCoords,
+            lng: lngCoords
+        }
     };
-    setPosition();
+    setPosition(pos);
     addMarker(pos);
 }
 
@@ -55,23 +133,41 @@ function initMap() {
 
     var infoWindow = new google.maps.InfoWindow;
 
-    setPosition = function () {
-        map.setCenter(pos);
-        x.innerHTML = "Latitude: " + latCoords +
-            "<br>Longitude: " + lngCoords;
+    setPosition = function (props) {
+        map.setCenter(props.pos);
+        x.innerHTML = props.coords.lat;
+        y.innerHTML = props.coords.lng;
     }
 
     //Add Marker Function
-    addMarker = function (pos) {
-        console.log(pos)
+    addMarker = function (props) {
         var marker = new google.maps.Marker({
-            position: pos,
+            position: props.coords,
             map: map
         });
+        markers.push(marker);
 
+        if (props.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
 
-        marker.addListener('click', function () {
-            infoWindow.open(map, marker);
-        })
+            marker.addListener('click', function () {
+                infoWindow.open(map, marker);
+            })
+        }
     }
+
+    // Sets the map on all markers in the array.
+    setMapOnAll = function (map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+    deleteMarkers = function () {
+        setMapOnAll(null);
+        markers = [];
+    }
+
 }
